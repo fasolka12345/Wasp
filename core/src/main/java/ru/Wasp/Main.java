e92fd833f967d77ac4f8d763440d39865fd7eefc
ulya <149610916+fasolka12345@users.noreply.github.com>
2024-11-15 17:45:19 +0300

    Version 2.0

--- core/src/main/java/ru/Wasp/Main.java
+++ core/src/main/java/ru/Wasp/Main.java
@@ -1,16 +1,22 @@
 package ru.Wasp;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    public static final float SCR_WIDTH = 1280;
+    public static final float SCR_HEIGHT = 720;
     private SpriteBatch batch;
     private Texture image;
+    float x = 0;
+    float y = 100;
+    float width = 378;
+    float height = 63;
+    float stepX = 5;
+    float stepY = 3;
 
     @Override
     public void create() {
@@ -20,9 +26,13 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
+        x += stepX;
+        y += stepY;
+        if (x > SCR_WIDTH - width|| x < 0) stepX = -stepX;
+        if (y > SCR_HEIGHT - height|| y < 0) stepY = -stepY;
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, x, y);
         batch.end();
     }
 
