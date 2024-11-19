5d629959210938880a628db49c80679d6bd2dbd8
ulya <149610916+fasolka12345@users.noreply.github.com>
2024-11-19 17:43:02 +0300

    Version 4.0

--- core/src/main/java/ru/Wasp/Wasp.java
+++ core/src/main/java/ru/Wasp/Wasp.java
@@ -4,17 +4,26 @@ import static ru.Wasp.Main.SCR_HEIGHT;
 import static ru.Wasp.Main.SCR_WIDTH;
 
 public class Wasp {
-    float x = 0;
-    float y = 100;
+    float x;
+    float y;
     float width = 200;
     float height = 200;
     float stepX = 5;
     float stepY = 3;
+
+    Wasp(float x1, float y) {
+        x = x1;
+        this.y = y;
+    }
 
     void fly() {
         x += stepX;
         y += stepY;
         if (x > SCR_WIDTH - width|| x < 0) stepX = -stepX;
         if (y > SCR_HEIGHT - height|| y < 0) stepY = -stepY;
+    }
+
+    boolean flip(){
+        return stepX < 0;
     }
 }
