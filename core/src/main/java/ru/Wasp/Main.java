85234a1dc207861b384c95d795ee315753bace70
ulya <149610916+fasolka12345@users.noreply.github.com>
2024-11-21 17:08:49 +0300

    Version 6.0

--- core/src/main/java/ru/Wasp/Main.java
+++ core/src/main/java/ru/Wasp/Main.java
@@ -1,36 +1,56 @@
 package ru.Wasp;
 
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     public static final float SCR_WIDTH = 1280;
     public static final float SCR_HEIGHT = 720;
+
     private SpriteBatch batch;
-    private Texture image;
-    Wasp[] wasp = new Wasp[33];
+    private Texture imgWasp;
+    private Texture imgBackGround;
+    private Texture imgTrump;
+
+    private Wasp[] wasp = new Wasp[33];
+    private Trump[] trump = new Trump[55];
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("Wasp.png");
+        imgWasp = new Texture("Wasp.png");
+        imgBackGround = new Texture("bg.png");
+        imgTrump = new Texture("Trump.png");
+
         for(int i = 0; i < wasp.length; i++) {
             wasp[i] = new Wasp(SCR_WIDTH/2, SCR_HEIGHT/2);
+        }
+        for(int i = 0; i < trump.length; i++) {
+            trump[i] = new Trump(0, 0);
         }
     }
 
     @Override
     public void render() {
+        // Независимые события
         for(int i = 0; i < wasp.length; i++) {
             wasp[i].fly();
         }
-        ScreenUtils.clear(1f, 1f, 1f, 1f);
+        for (int i = 0; i < trump.length; i++) {
+            trump[i].fly();
+        }
+
+        //Отрисовка
         batch.begin();
+        batch.draw(imgBackGround, 0, 0, SCR_WIDTH, SCR_HEIGHT);
         for(int i = 0; i < wasp.length; i++) {
-            batch.draw(image, wasp[i].x, wasp[i].y, wasp[i].width, wasp[i].height, 0, 0, 789, 575, wasp[i].flip(), false);
+            batch.draw(imgWasp, wasp[i].x, wasp[i].y, wasp[i].width, wasp[i].height, 0, 0, 789, 575, wasp[i].flip(), false);
+        }
+        for (int i = 0; i < trump.length; i++) {
+            batch.draw(imgTrump, trump[i].x, trump[i].y, trump[i].width, trump[i].height, 0, 0, 250, 200, trump[i].flip(), false);
         }
         batch.end();
     }
@@ -38,6 +58,8 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        imgBackGround.dispose();
+        imgWasp.dispose();
+        imgTrump.dispose();
     }
 }
--- core/src/main/java/ru/Wasp/Wasp.java
+++ core/src/main/java/ru/Wasp/Wasp.java
@@ -1,34 +1,8 @@
 package ru.Wasp;
 
-import static ru.Wasp.Main.SCR_HEIGHT;
-import static ru.Wasp.Main.SCR_WIDTH;
-
 import com.badlogic.gdx.math.MathUtils;
 
-public class Wasp {
-    float x;
-    float y;
-    float width = 200;
-    float height = 200;
-    float stepX = 5;
-    float stepY = 3;
-
-    Wasp(float x, float y) {
-        this.x = x;
-        this.y = y;
-        width = height = MathUtils.random(50, 150);
-        stepX = MathUtils.random(-5, 5);
-        stepY = MathUtils.random(-5, 5);
-    }
-
-    void fly() {
-        x += stepX;
-        y += stepY;
-        if (x > SCR_WIDTH - width|| x < 0) stepX = -stepX;
-        if (y > SCR_HEIGHT - height|| y < 0) stepY = -stepY;
-    }
-
-    boolean flip(){
-        return stepX < 0;
-    }
+public class Wasp extends Insect{
+    public Wasp(float x, float y) {
+        super(x, y);
 }
